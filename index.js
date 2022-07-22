4f445b24008e0f1c996e7f6bfbebf7804500f9fc
John Goh <johngohrw@gmail.com>
2022-07-22 20:01:36 +0800

    generation algorithm

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// rng. if max = 4, values will be either 0, 1, 2, or 3
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -6,6 +7,7 @@ function coordToSection(x, y) {
   return Math.floor(y / 3) * 3 + Math.floor(x / 3);
 }
 
+// fast random shuffle for arrays
 function fisherYatesShuffle(array) {
   let m = array.length,
     t,
@@ -26,158 +28,133 @@ function fisherYatesShuffle(array) {
 }
 
 class SudokuBoard {
-  constructor(debug = false, difficulty = 5) {
-    this.debug = debug;
-    this.validRows = this.newValidObj();
-    this.validCols = this.newValidObj();
-    this.validSections = this.newValidObj();
-    this.board = this.newBoardWithZeroes();
-    // this.newBoard(difficulty);
+  constructor(difficulty = 5) {
+    this.originalBoard;
+    this.board;
+    this.solution;
+    this.newGame(difficulty);
   }
 
-  newBoardWithZeroes() {
-    let board = [];
-    for (let i = 0; i < 9; i++) {
-      let row = [];
-      for (let j = 0; j < 9; j++) {
-        row.push(0);
-      }
-      board.push(row);
-    }
-    return board;
+  newGame(difficulty = 5) {
+    let [board, solution] = this.generateBoard(difficulty);
+    this.originalBoard = JSON.parse(JSON.stringify(board)); // this method of deepcopy is probably not sustainable
+    this.board = JSON.parse(JSON.stringify(board)); // this method of deepcopy is probably not sustainable
+    this.solution = solution;
   }
 
-  // difficulty is a 0-10 rating
-  newBoard(difficulty = 0) {
+  generateBoard(difficulty = 5) {
     if (difficulty < 0 || difficulty > 10) {
       throw Error(
         `Invalid difficulty setting (${difficulty}), keep it within 0-10`
       );
     }
+    let board = this.generateFullBoard();
+    const solution = board.map((o) => ({
+      id: o.id,
+      x: o.x,
+      y: o.y,
+      value: o.value,
+    }));
+    // difficulty setting by removing cells randomly
     let emptySpaces = Math.floor(81 * ((difficulty * 10) / 100));
-    let numQueue = this.newNumberQueue();
-    let insertCount = 81 - emptySpaces;
-    if (this.debug) {
-      console.log(
-        `Creating a new board with a difficulty of ${difficulty}/10 (${insertCount} filled cells)..`
-      );
+    board = fisherYatesShuffle(board);
+    for (let i = 0; i < emptySpaces; i++) {
+      board[i].value = 0;
     }
-    let counter = 0;
-    while (counter < insertCount) {
-      console.log(numQueue[counter]);
-      this.insertNumber(numQueue[counter]);
-      counter++;
-    }
+    board = board.sort((a, b) => a.id - b.id);
+    return [board, solution];
   }
 
-  newValidObj() {
-    function getNewValids() {
-      let valids = [];
-      for (let i = 0; i < 9; i++) {
-        valids.push(i);
-      }
-      return valids;
-    }
-    let obj = {};
-    for (let i = 1; i < 10; i++) {
-      obj[i] = getNewValids();
-    }
-    return obj;
-  }
+  generateFullBoard() {
+    let n = 0;
+    let board;
+    // keep trying to generate a full board. n won't hit 80 if it's not a full board.
+    while (n < 80) {
+      board = this.newEmptyBoard();
+      n = 0;
+      while (n < 81) {
+        // filter out nonpossible cells, pre-randomize, then sort based on possible length
+        let filtered = board.filter((o) => o.possible.length !== 0);
+        let randomized = fisherYatesShuffle(filtered);
+        let sorted = randomized.sort(
+          (a, b) => a.possible.length - b.possible.length
+        );
+        // get cell with least possibilities (sorted first)
+        let cell = sorted[0];
 
-  newNumberQueue() {
-    let queue = [];
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        queue.push(i + 1);
-      }
-    }
-    // return fisherYatesShuffle(queue);
-    return queue;
-  }
-
-  getValidCoords(n) {
-    let validRows = this.validRows[n];
-    let validCols = this.validCols[n];
-    let validSections = this.validSections[n];
-    let validCoordList = [];
-    validRows.forEach((row) => {
-      validCols.forEach((col) => {
-        let x = col;
-        let y = row;
-        if (this.board[y][x] === 0) {
-          validCoordList.push([x, y]);
+        // if an impossible situation is met, start over from an empty board.
+        if (!cell) {
+          break;
         }
-      });
-    });
-    validCoordList = validCoordList.filter(([x, y]) => {
-      return validSections.indexOf(coordToSection(x, y)) >= 0;
-    });
-    return validCoordList;
+
+        // get current cell's x, y, and section
+        const [x, y, section] = [
+          cell.x,
+          cell.y,
+          coordToSection(cell.x, cell.y),
+        ];
+
+        // get num randomly from a list of possibilities
+        let num = cell.possible[getRandomInt(cell.possible.length)];
+
+        let index = board.findIndex((o) => o.id === cell.id);
+        board[index].value = num; // this cell now has a value
+        board[index].possible = []; // this cell is now non-possible
+
+        // find all cells with the same x, y, or section
+        let similarCells = board.filter(
+          (cell) => cell.x === x || cell.y === y || cell.section === section
+        );
+
+        // for each affected cell, filter out `num` from its possible array
+        similarCells.forEach((cell) => {
+          cell.possible = cell.possible.filter((_num) => _num !== num);
+        });
+        n++;
+      }
+    }
+    return board;
   }
 
-  insertNumber(n) {
-    let validCoords = this.getValidCoords(n);
-    if (validCoords.length <= 0) {
-      throw Error(
-        `no valid coordinates for number ${n} for board: \n${this.getBoardString()}`
-      );
+  newEmptyBoard() {
+    let board = [];
+    let i = 0;
+    while (i < 81) {
+      board.push({
+        id: i,
+        x: i % 9,
+        y: Math.floor(i / 9),
+        section: coordToSection(i % 9, Math.floor(i / 9)),
+        possible: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        value: 0,
+      });
+      i++;
     }
-    let [x, y] = validCoords[getRandomInt(validCoords.length)];
-    if (this.debug) {
-      console.log(`inserting ${n} at coords ${x}, ${y}`);
-    }
-    this.board[y][x] = n;
-    this.validRows[n] = this.validRows[n].filter((o) => o !== y);
-    this.validCols[n] = this.validCols[n].filter((o) => o !== x);
-    let section = coordToSection(x, y);
-    this.validSections[n] = this.validSections[n].filter((o) => o !== section);
+    return board;
   }
 
-  getBoardString() {
-    let string = `     0 1 2   3 4 5   6 7 8\n`;
-    string += "   -------------------------\n";
-    this.board.forEach((row, rowIdx) => {
-      let rowString = `${rowIdx}  | `;
-      row.forEach((col, colIdx) => {
-        rowString += `${col === 0 ? " " : col} ${
-          (colIdx + 1) % 3 === 0 ? "| " : ""
-        }`;
-      });
-      string += `${rowString}\n`;
-      if ((rowIdx + 1) % 3 === 0) {
-        string += "   -------------------------\n";
+  getBoardString(board) {
+    let string = `    0 1 2   3 4 5   6 7 8\n`;
+    string += "  -------------------------\n";
+    board.forEach((cell) => {
+      if (cell.id % 9 === 0) {
+        string += `${Math.floor(cell.id / 9)} | `;
+      }
+      string += `${cell.value === 0 ? " " : cell.value} `;
+      if (cell.id % 3 === 2) {
+        string += `| `;
+      }
+      if (cell.id % 9 === 8) {
+        string += `\n`;
+        if (Math.floor(cell.id / 9) % 3 === 2) {
+          string += "  -------------------------\n";
+        }
       }
     });
     return string;
   }
 
   printBoard() {
-    console.log(this.getBoardString());
+    console.log(this.getBoardString(this.board));
   }
 }
-
-// let board = newBoard();
-// let coordCounter = newCoordCounter();
-// let numbers = newNumberQueue();
-// let sections = newSections();
-
-// coordCounter["x"][0] -= 1;
-// coordCounter["y"][3] -= 1;
-// console.log(coordToSection(6, 0));
-
-let board = new SudokuBoard(true, 0);
-let n = 1;
-while (n <= 3) {
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  board.insertNumber(n);
-  n++;
-}
-board.printBoard();
